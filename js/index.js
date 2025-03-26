078873e88ad7896dd1b704ca43bf1e1be0521678
JakeFaridoni5 <jafitup@gmail.com>
2025-03-26 17:33:42 +0000

    Wed, Mar 26, 2025, 10:33 AM -07:00

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ const backImg = document.querySelector('.hero')
 
 document.addEventListener('scroll', () => {
     backImg.style.filter = `blur(${window.scrollY * 0.05}px)`
-    // textBox.style.top = `-${window.scrollY}px`
-    if (parseInt(textBox.style.top) > -205)
-    {
-        textBox.style.top = `-${window.scrollY}px`
-    }
-    else {
-        textBox.style.top = `-205px`
-    }
-    console.log(textBox)
+    textBox.style.top = `-${window.scrollY}px`
+    // if (parseInt(textBox.style.top) > -205)
+    // {
+    //     textBox.style.top = `-${window.scrollY}px`
+    // }
+    // else {
+    //     textBox.style.top = `-205px`
+    // }
+    // console.log(textBox)
 })

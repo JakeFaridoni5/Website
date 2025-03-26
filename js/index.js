d1c0e74a43f554abd410d0df679107f92beeae1d
JakeFaridoni5 <jafitup@gmail.com>
2025-03-26 17:03:01 +0000

    Wed, Mar 26, 2025, 10:03 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const 
+const textBox = document.querySelector('.text')
 const backImg = document.querySelector('.hero')
 
 document.addEventListener('scroll', () => {
     backImg.style.filter = `blur(${window.scrollY * 0.05}px)`
-
+    textBox.style.top = `-${window.scrollY}px`
 })

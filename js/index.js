a4aa3b07629d6a69fb15b772dc2910c7a1334a28
JakeFaridoni5 <jafitup@gmail.com>
2025-03-26 16:55:02 +0000

    Wed, Mar 26, 2025, 9:55 AM -07:00

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const listAni = document.querySelector('.ul')
 const backImg = document.querySelector('.hero')
 
 document.addEventListener('scroll', () => {
-    
+    backImg.style.filter = `blur(${window.scrollY * 0.05}px)`
 })

7d81ab3ffe5bc635a30aeb6d1072d97a36e973b9
JakeFaridoni5 <jafitup@gmail.com>
2025-03-27 01:12:34 +0000

    Wed, Mar 26, 2025, 6:12 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 const textBox = document.querySelector('.textBox')
 const backImg = document.querySelector('.hero')
+const navBar = document.querySelector('.nav')
 
 document.addEventListener('scroll', () => {
     backImg.style.filter = `blur(${window.scrollY * 0.05}px)`
     textBox.style.top = `-${window.scrollY}px`
-    // if (parseInt(textBox.style.top) > -205)
-    // {
-    //     textBox.style.top = `-${window.scrollY}px`
-    // }
-    // else {
-    //     textBox.style.top = `-205px`
-    // }
-    // console.log(textBox)
+    if (parseInt(textBox.style.top) >= -205)
+    {
+        textBox.style.top = `-${window.scrollY}px`
+        navBar.classList.remove('active')
+    }
+    else {
+        textBox.style.top = `-205px`
+        navBar.classList.add('active')
+    }
 })

a618487aa4c5ac6333d8b4881831ce650f810d77
JakeFaridoni5 <jafitup@gmail.com>
2025-04-09 17:09:05 +0000

    Wed, Apr 9, 2025, 10:09 AM -07:00

--- js/pong.js
+++ js/pong.js
@@ -213,10 +213,8 @@ function dif4() {
 }
 
 function dif5() {
-    // ballSpeed = ballSpeed * 1.5;
-    // paddleSpeed = paddleSpeed * 1.5;
-    ballSpeed = 5;
-    paddleSpeed = 1.5 * ballSpeed;
+    ballSpeed = ballSpeed * 1.5;
+    paddleSpeed = paddleSpeed * 1.5;
 
     ball.style.opacity = "0.2";
     ball.style.transition = "background-color 1.5s ease";

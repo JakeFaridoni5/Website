426c9ba8e382e52af3ef58c681f60fc5435db065
JakeFaridoni5 <jafitup@gmail.com>
2025-04-09 16:17:55 +0000

    Wed, Apr 9, 2025, 9:17 AM -07:00

--- js/pong.js
+++ js/pong.js
@@ -6,15 +6,15 @@ const score = document.createElement('div')
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
 //
-let ballXPosition = windowWidth/2 -ballRadius
+let ballXPosition = windowWidth / 2 - ballRadius
 let ballSpeed = 5;
 let ballXDirection = 1
-let ballYPosition = windowHeight/2 - ballRadius
+let ballYPosition = windowHeight / 2 - ballRadius
 let ballYDirection = -1
 //
 let paddleWidth = 12 * ballRadius
 let paddleHeight = 35
-let paddleXPosition = windowWidth/2 - paddleWidth/2
+let paddleXPosition = windowWidth / 2 - paddleWidth / 2
 let paddleYPosition = 55
 let paddleSpeed = 1.5 * ballSpeed
 let paddleDirection = 0
@@ -27,7 +27,7 @@ setInterval(moveBall, 10)
 setInterval(movePaddleRight, 10)
 setInterval(movePaddleLeft, 10)
 
-function createBall(){
+function createBall() {
     document.body.appendChild(ball)
     ball.style.height = `${2 * ballRadius}px`
     ball.style.width = `${2 * ballRadius}px`
@@ -38,7 +38,7 @@ function createBall(){
     ball.style.left = `${ballXPosition}px`
 }
 
-function createPaddle(){
+function createPaddle() {
     document.body.appendChild(paddle)
     paddle.style.height = `${paddleHeight}px`
     paddle.style.width = `${paddleWidth}px`
@@ -60,32 +60,32 @@ function createScore() {
     score.style.zIndex = '-1';
 }
 
-    function moveBall(){
-        ballXPosition = ballXPosition + ballSpeed * ballXDirection
-        ball.style.left = `${ballXPosition}px`
-        if (ballXPosition < 0 || ballXPosition > (windowWidth - 2 * ballRadius)){
-            ballXDirection = ballXDirection * -1
-        }
-        ballYPosition = ballYPosition + ballSpeed * ballYDirection
-        ball.style.top = `${ballYPosition}px`
-        if (ballYPosition < 0 || ballYPosition > (windowHeight - 2 * ballRadius)){
-            ballYDirection = ballYDirection * -1
-        }
-        let ballTop = ballYPosition;
-        let ballBottom = ballYPosition + 2 * ballRadius;
-        let ballLeft = ballXPosition;
-        let ballRight = ballXPosition + 2 * ballRadius;
-        let paddleTop = paddleYPosition;
-        let paddleBottom = paddleYPosition + paddleHeight;
-        let paddleLeft = paddleXPosition;
-        let paddleRight = paddleXPosition + paddleWidth;
-        if (
-            ballBottom >= paddleTop &&
-            ballTop <= paddleBottom &&
-            ballLeft <= paddleRight &&
-            ballRight >= paddleLeft &&
-            ballYDirection === -1
-        ) {
+function moveBall() {
+    ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ball.style.left = `${ballXPosition}px`
+    if (ballXPosition < 0 || ballXPosition > (windowWidth - 2 * ballRadius)) {
+        ballXDirection = ballXDirection * -1
+    }
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > (windowHeight - 2 * ballRadius)) {
+        ballYDirection = ballYDirection * -1
+    }
+    let ballTop = ballYPosition;
+    let ballBottom = ballYPosition + 2 * ballRadius;
+    let ballLeft = ballXPosition;
+    let ballRight = ballXPosition + 2 * ballRadius;
+    let paddleTop = paddleYPosition;
+    let paddleBottom = paddleYPosition + paddleHeight;
+    let paddleLeft = paddleXPosition;
+    let paddleRight = paddleXPosition + paddleWidth;
+    if (
+        ballBottom >= paddleTop &&
+        ballTop <= paddleBottom &&
+        ballLeft <= paddleRight &&
+        ballRight >= paddleLeft &&
+        ballYDirection === -1
+    ) {
         ballYDirection = ballYDirection * -1;
         increaseScore();
     }
@@ -100,40 +100,38 @@ function createScore() {
     }
 }
 
-    function increaseScore() {
-        score.innerHTML = `${parseInt(score.innerHTML) + 1}`;
+function increaseScore() {
+    score.innerHTML = `${parseInt(score.innerHTML) + 1}`;
+}
+
+function movePaddleRight() {
+    if (paddleDirection == 1 && paddleXPosition + paddleWidth <= windowWidth - 1) {
+        paddleXPosition = paddleXPosition + paddleSpeed
+        paddle.style.left = `${paddleXPosition}px`
     }
+}
 
-    function movePaddleRight(){
-        if (paddleDirection == 1 && paddleXPosition + paddleWidth <= windowWidth - 1)
-        {
-            paddleXPosition = paddleXPosition + paddleSpeed
-            paddle.style.left = `${paddleXPosition}px`
-        }
+function movePaddleLeft() {
+    if (paddleDirection == -1 && paddleXPosition >= 0) {
+        paddleXPosition = paddleXPosition - paddleSpeed
+        paddle.style.left = `${paddleXPosition}px`
     }
+}
 
-    function movePaddleLeft(){
-        if (paddleDirection == -1 && paddleXPosition >= 0)
-        {
-            paddleXPosition = paddleXPosition - paddleSpeed
-            paddle.style.left = `${paddleXPosition}px`
-        }
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {
+        paddleDirection = 1
     }
+    if (event.key == 'ArrowLeft') {
+        paddleDirection = -1
+    }
+})
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'ArrowRight'){
-            paddleDirection = 1
-        }
-        if (event.key == 'ArrowLeft'){
-            paddleDirection = -1
-        }
-    })
-
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'ArrowRight'){
-            paddleDirection = 0
-        }
-        if (event.key == 'ArrowLeft'){
-            paddleDirection = 0
-        }
-    })
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'ArrowRight') {
+        paddleDirection = 0
+    }
+    if (event.key == 'ArrowLeft') {
+        paddleDirection = 0
+    }
+})
